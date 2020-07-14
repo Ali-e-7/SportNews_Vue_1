39d8e8a3cc0fe0cdf099af7baa7c63ae46b040a5
unknown <aliebrahimi13710@gamil.com>
2020-07-14 17:06:25 +0430

    added vue-resourse

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
+import VueResource from 'vue-resource';
 import App from './App.vue';
 
 
@@ -8,6 +9,7 @@ import { RoutNews } from './Routes';
 
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 
 const router = new VueRouter({
